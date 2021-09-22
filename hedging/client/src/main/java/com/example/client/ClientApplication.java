06179b717cc5aea6a982a8fe24f211ec891bb076
Josh Long <josh@joshlong.com>
2021-09-21 17:31:35 -0700

    Update ClientApplication.java

--- hedging/client/src/main/java/com/example/client/ClientApplication.java
+++ hedging/client/src/main/java/com/example/client/ClientApplication.java
@@ -1,43 +1,67 @@
 package com.example.client;
 
-import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.*;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.net.URI;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.HashSet;
 
 @SpringBootApplication
 public class ClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
 
-	@Bean
-	WebClient client(HedgeExchangeFilterFunction eff) {
-		return WebClient.builder().filter(eff).build();
-	}
+    @Bean
+    ApplicationRunner applicationRunner(DiscoveryClient dc) {
+        return args -> dc.getInstances("service")
+                .forEach(si -> System.out.println("new service: " + si.getServiceId() + ':' + si.getPort()));
+    }
+/*
 
+    @LoadBalanced
+    @Bean
+    WebClient.Builder builder() {
+        return WebClient.builder();
+    }
+*/
+
+    @Bean
+    WebClient webClient(WebClient.Builder b) {
+        return b.build();
+    }
 }
 
+@Controller
+@ResponseBody
+class ClientRestController {
 
+    private final WebClient http;
+    private final LoadBalancerClient loadBalancerClient;
+    private final DiscoveryClient discoveryClient;
 
-/ *         var max = 2;
+    ClientRestController(DiscoveryClient dc, WebClient http, LoadBalancerClient loadBalancerClient) {
+        this.http = http;
+        this.discoveryClient = dc;
+        this.loadBalancerClient = loadBalancerClient;
+    }
+
+    @GetMapping("/client")
+    Mono<String> helloClient() {
+
+        var max = 2;
         var serviceInstances = this.discoveryClient.getInstances("service");
         Assert.state(serviceInstances.size() >= 2, () -> "there should be " +
                                                          "at least two instances in the load balancer!");
@@ -48,94 +72,17 @@ public class ClientApplication {
             chosenServiceInstances.add(serviceInstances.get(index));
             System.out.println("there are " + chosenServiceInstances.size() + " elements.");
         }
-        var requests = Flux.fromIterable(chosenServiceInstances)
-                .flatMap(si -> http.get()
-                        .uri(si.getScheme() + "://" + si.getHost() + ':' + si.getPort()).retrieve().bodyToMono(String.class));
-        return Flux.firstWithSignal(requests).singleOrEmpty();
- */
-@Log4j2
-@Component
-class HedgeExchangeFilterFunction
-	implements ExchangeFilterFunction {
+        var requests = Flux
+                .fromIterable(chosenServiceInstances)
+                .flatMap(this::withServiceInstance);
+        return Flux.firstWithSignal(requests)
+                .take(1)
+                .singleOrEmpty()
+                .doOnNext(s -> System.out.println("result " + s));
 
-	private final DiscoveryClient discoveryClient;
-	private final LoadBalancerClient loadBalancerClient;
-	private final int attempts, maxAttempts;
+    }
 
-	@Autowired
-	HedgeExchangeFilterFunction(DiscoveryClient dc, LoadBalancerClient lbc) {
-		this(dc, lbc, 3);
-	}
-
-	HedgeExchangeFilterFunction(
-		DiscoveryClient discoveryClient,
-		LoadBalancerClient loadBalancerClient,
-		int attempts) {
-		this.discoveryClient = discoveryClient;
-		this.loadBalancerClient = loadBalancerClient;
-		this.attempts = attempts;
-		this.maxAttempts = attempts * 2;
-	}
-
-	@Override
-	public Mono<ClientResponse> filter(ClientRequest request, ExchangeFunction next) {
-
-		URI originalURI = request.url();
-		String serviceId = originalURI.getHost();
-		List<ServiceInstance> serviceInstanceList = this.discoveryClient.getInstances(serviceId);
-		Assert.state(serviceInstanceList.size() >= this.attempts, "there must be at least " +
-			this.attempts + " instances of the service " + serviceId + "!");
-		int counter = 0;
-		Map<String, Mono<ClientResponse>> ships = new HashMap<>();
-		while (ships.size() < this.attempts && (counter++ < this.maxAttempts)) {
-			ServiceInstance lb = this.loadBalancerClient.choose(serviceId);
-			String asciiString = lb.getUri().toASCIIString();
-			ships.computeIfAbsent(asciiString, str -> this.invoke(lb, originalURI, request, next));
-		}
-		return Flux
-			.first(ships.values())
-			.singleOrEmpty();
-	}
-
-	private Mono<ClientResponse> invoke(ServiceInstance serviceInstance,
-																																					URI originalURI,
-																																					ClientRequest request,
-																																					ExchangeFunction next) {
-
-		URI uri = this.loadBalancerClient.reconstructURI(serviceInstance, originalURI);
-		ClientRequest newRequest = ClientRequest
-			.create(request.method(), uri)
-			.headers(h -> h.addAll(request.headers()))
-			.cookies(c -> c.addAll(request.cookies()))
-			.attributes(a -> a.putAll(request.attributes()))
-			.body(request.body())
-			.build();
-
-		return next
-			.exchange(newRequest)
-			.doOnNext(cr -> log.info("launching " + newRequest.url()));
-	}
-}
-
-@RestController
-class HedgingRestController {
-
-	private final WebClient client;
-
-	HedgingRestController(WebClient client) {
-		this.client = client;
-	}
-
-	@GetMapping("/hedge")
-	Flux<String> greet() {
-		return this.client
-			.get()
-			.uri("http://service/hi")
-			.retrieve()
-			.bodyToFlux(String.class);
-
-
-
-	}
-
+    Mono<String> withServiceInstance(ServiceInstance si) {
+        return http.get().uri(si.getScheme() + "://" + si.getHost() + ':' + si.getPort() + "/hello").retrieve().bodyToMono(String.class);
+    }
 }
